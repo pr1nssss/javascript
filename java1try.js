5f1ca4bbadff5f445e7db1f1d2ce7d1704b1335a
pr1ns <prinslumbres@gmail.com>
2022-06-07 12:55:26 +0800

    day2

---
+++ java1try.js
@@ -0,0 +1,20 @@
+function answer1() {
+    document.getElementById('answer1').innerHTML = 5 + 6
+}
+
+function answer2() {
+    document.getElementById('answer2').innerHTML = 13 * 27
+}
+
+function answer3() {
+    window.alert(972/23)
+}
+
+function answer4() {
+
+    let a,b,c;
+    a = 100;
+    b = 899;
+    c = a + b;
+    document.getElementById('answer4').innerHTML = c
+}
